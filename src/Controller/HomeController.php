5d5f140e4b1fa8a1c9de04bc394e3da13ece3ea5
lkieffer <lea57.kieffer@gmail.com>
2019-10-22 10:04:44 +0200

    sauvegarde cinq

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,13 +8,24 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends Controller
 {
     /**
-    * @Route("/acceuil", name="acceuil")
+    * @Route("/", name="accueil2")
+    * @Route("/accueil", name="accueil")
     */
     public function index()
     {
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
-    }
+    }   
+
+    /**
+    * @Route("/apropos", name="apropos")
+    */
+    public function a_propos()
+    {
+        return $this->render('home/a_propos.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }   
 }
 
