8ee2f01836d8809d9b1add9da4980a57a5c67842
lkieffer <lea57.kieffer@gmail.com>
2019-11-06 15:55:11 +0100

    boucle imbriquéer

--- src/DataFixtures/ArticleFixtures.php
+++ src/DataFixtures/ArticleFixtures.php
@@ -5,6 +5,8 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use App\Entity\Article;
+use App\Entity\Categorie;
+use App\Entity\Commentaire;
 
 class ArticleFixtures extends Fixture
 {
@@ -13,20 +15,40 @@ class ArticleFixtures extends Fixture
         // $product = new Product();
         // $manager->persist($product);
         
-    /* for($i=0; $i<200; $i++)
-        {
-            $article = new Article();
-            $article ->setTitle('article n°')
-                     ->setContent('contenue n°')
-                     ->setImage('image n°')
-                     ->setCreatedAt(new \DateTime());
+        for($i=0; $i<3; $i++)
+        {   
+            $categorie = new Categorie();
+            $categorie ->setTitre ('titre n°')
+                       ->setResumer ('resumer n°');
+        
+        $manager ->persist($categorie);
+    
+            for($j=0; $j<10; $j++)
+            {
+                $article = new Article();
+                $article ->setTitle('article n°')
+                         ->setContent('contenue n°')
+                         ->setImage('image n°')
+                         ->setCreatedAt(new \DateTime())
+                         ->setCategorie ($categorie);
             
             $manager->persist($article);
         
-        }
-     
-    $manager->flush(); */
+                for($k=0; $k<5; $k++)
+                {
+                    $commentaire = new commentaire();
+                    $commentaire ->setAuteur('auteur')
+                                 ->setCommentaire('Commentaire')
+                                 ->setCreatedAt(new \DateTime())
+                                 ->setArticle ($article);
 
+                $manager->persist($commentaire);
+
+                }
+            }
+        }
+
+    $manager->flush(); 
         
     }
     
