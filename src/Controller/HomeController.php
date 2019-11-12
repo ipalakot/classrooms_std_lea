1ddc2e251ca1cd060fad2de12c8577db3d4b180e
lkieffer <lea57.kieffer@gmail.com>
2019-11-12 12:09:26 +0100

    article en colone

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,9 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Article;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 
 class HomeController extends Controller
 {
@@ -11,10 +14,18 @@ class HomeController extends Controller
     * @Route("/", name="accueil2")
     * @Route("/accueil", name="accueil")
     */
-    public function index()
+    public function index(PaginatorInterface $paginator, Request $request)
     {
+        $repo = $this->getDoctrine()->getRepository(Article::class);
+        $articles =  $paginator->paginate(
+            $repo->findAll(), 
+            $request->query->getInt('page', 1), /*page number*/
+            15 /*limit per page*/
+        );
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'articles'=> $articles
         ]);
     }   
 
