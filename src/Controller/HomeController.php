8ff8348b56aeb4e9b0cf67ad698787136898d9c9
lkieffer <lea57.kieffer@gmail.com>
2019-11-13 08:50:41 +0100

    rooting

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -61,7 +61,7 @@ class HomeController extends Controller
     }  
 
     /**
-    * @Route("/admin", name="admin")
+    * @Route("/admin/admin", name="admin")
     */
     public function admin()
     {
